e2270df048a9ba21aa3e7a385c6976f50c9338cd
ddimkou <ddimkou@outlook.com>
2023-06-09 03:30:45 +0300

    +left feed comp

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
 
 function Feed() {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2023 Dimkou Dimitrios
+        </Typography>
+      </Box>
+    </Stack>
+  );
 }
 
 export default Feed;
