7a2d2bb0f0bc569445a1a4d4be83c583644fbf51
ddimkou <ddimkou@outlook.com>
2023-06-10 03:22:17 +0300

    added fetch, updated feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+
+// import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Sidebar, Videos } from "./";
 
 function Feed() {
+  // const [selectedCategory, setSelectedCategory] = useState("New");
+
+  // useEffect(() => {
+  //   fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+
+  //   return () => {
+  //     second;
+  //   };
+  // }, []);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
@@ -20,6 +32,17 @@ function Feed() {
         >
           Copyright 2023 Dimkou Dimitrios
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overlowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
